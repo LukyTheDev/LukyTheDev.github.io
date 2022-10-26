3b12e987fb208a45cba9b0be92c17d5f9fa1a6b7
LukyTheDev <lucaian35@gmail.com>
2022-10-26 19:25:39 +0200

    via

--- dbdPage.js
+++ dbdPage.js
@@ -1,3 +1,70 @@
+/*****************typewriter animation */
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = () => {
+    setTimeout(
+         () => {
+            var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #dac6ac}";
+    document.body.appendChild(css);
+            }, 2600
+        )
+    } 
+        
+    
+
+
+
+
 /* hamburger animation */
 
 const hamburger = document.querySelector('.hamburger')
@@ -31,15 +98,11 @@ hamburger.addEventListener('click', () => {
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    // aniamzione loader
+    // animazione loader
     setTimeout(() => {
         document.querySelector('body').style.overflow = "visible"
         document.querySelector('.loader').classList.add('nOpacity')
     }, 2550)
-
-    // preset scritta 
-    categoria.innerHTML = `${secondContentInner[0].categoria}`
-    descrizione.innerHTML = `${secondContentInner[0].descrizione}`
 })
 
 window.addEventListener('resize', ()=> {
